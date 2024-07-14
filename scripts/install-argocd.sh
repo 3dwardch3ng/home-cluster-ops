63dea78deb477207dad375ad88ef614b5e715738
Edward Cheng <edward@cheng.sydney>
2024-07-14 19:28:21 +1000

    update script

--- scripts/install-argocd.sh
+++ scripts/install-argocd.sh
@@ -3,4 +3,5 @@ set -e
 
 kubectl create namespace argocd || echo "Namespace argocd already exists"
 helm repo add argo https://argoproj.github.io/argo-helm || echo "Argo repo already exists"
+helm repo update || echo "Failed to update helm repos"
 helm install argocd -n argocd -f argocd-values.yaml argo/argocd --version 7.3.6
