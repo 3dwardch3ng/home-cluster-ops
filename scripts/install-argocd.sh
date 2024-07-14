ee6201827a3492a9ab51594c96323729a45f5b54
Edward Cheng <edward@cheng.sydney>
2024-07-14 19:38:29 +1000

    update script

--- scripts/install-argocd.sh
+++ scripts/install-argocd.sh
@@ -4,7 +4,7 @@ set -e
 kubectl create namespace argocd || echo "Namespace argocd already exists"
 helm template argocd \
   oci://ghcr.io/argoproj/argo-helm/argo-cd \
-  --version: 7.3.6 \
+  --version 7.3.6 \
   --release-name argocd \
   --api-versions monitoring.coreos.com/v1 \
   --values argocd-values.yaml
