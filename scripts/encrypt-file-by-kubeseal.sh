7d11d2aa4cfe9541a5155e0874e6d9929c3cfe8a
Edward Cheng <edward@cheng.sydney>
2024-07-18 16:35:35 +1000

    update secrets

--- scripts/encrypt-file-by-kubeseal.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-set -e
-
-filePath=$1
-
-kubeseal --format=yaml --scope cluster-wide -n argocd --controller-name sealed-secrets --controller-namespace argocd < $filePath > $filePath
-
