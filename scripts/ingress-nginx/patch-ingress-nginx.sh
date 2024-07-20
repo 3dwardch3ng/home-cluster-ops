c11e7bb03a3d60a11d44506a0d04807ac40140df
Edward Cheng <edward@cheng.sydney>
2024-07-20 12:59:37 +1000

    update service ports adguard-home

--- scripts/ingress-nginx/patch-ingress-nginx.sh
+++ scripts/ingress-nginx/patch-ingress-nginx.sh
@@ -3,4 +3,4 @@ set -e
 
 serviceName=$1
 
-kubectl patch service ingress-nginx-controller -n ingress-nginx --patch "$(cat $serviceName.yaml)"
+kubectl patch service ingress-nginx-controller -n argocd --patch "$(cat $serviceName.yaml)"
