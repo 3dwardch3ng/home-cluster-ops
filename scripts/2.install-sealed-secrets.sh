c322a7627d344448292a544f315d1d4ecd3bc37d
Edward Cheng <edward@cheng.sydney>
2024-09-24 21:41:27 +1000

    update scripts

--- scripts/2.install-sealed-secrets.sh
+++ scripts/2.install-sealed-secrets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
-helm repo update
+helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets && \
+helm repo update && \
 helm upgrade --install sealed-secrets -n kube-system --set-string fullnameOverride=sealed-secrets-controller sealed-secrets/sealed-secrets -f sealed-secrets-values.yaml
