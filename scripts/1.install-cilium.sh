3f5313b84ec05d7fa53b70a4108624e7a283b5fc
Edward Cheng <edward.cheng@coatesgroup.com>
2025-03-31 16:17:56 +1100

    update installed versions of cillium and argocd

--- scripts/1.install-cilium.sh
+++ scripts/1.install-cilium.sh
@@ -6,7 +6,7 @@ sudo ip link delete cilium_net || true && \
 sudo ip link delete cilium_vxlan || true && \
 helm repo add cilium https://helm.cilium.io || echo "Cilium repo already exists" && \
 helm repo update || echo "Failed to update helm repos" && \
-helm upgrade --install cilium cilium/cilium --version 1.16.1 \
+helm upgrade --install cilium cilium/cilium --version 1.17.2 \
   --namespace kube-system --create-namespace \
   --set operator.replicas=2 \
   --set prometheus.enabled=true \
